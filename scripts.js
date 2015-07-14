dc972e089db837b6367063ef92c86315356f537d
Armando Amador <arm.amador@gmail.com>
2015-07-13 23:07:28 -0400

    Debug program to fix typos

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-alert("hello there");
-document.write("No it works!");
-console.log("Program complete");
+console.log("Start program");
+alert("Help me fix this program!");
+alert("Can you get this message to appear?");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>I'm practicing 'debugging'.</p>");
+console.log("End program");
