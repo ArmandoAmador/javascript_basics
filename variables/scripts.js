d45b61bba97f7a8acd557d519bd1ad007e0e833d
Armando Amador <arm.amador@gmail.com>
2015-07-14 23:01:29 -0400

    add variable folder
     - Work with single and double quotes values when creating variables

---
+++ variables/scripts.js
@@ -0,0 +1,9 @@
+var message = 'Hello';
+alert(message);
+message = "Goodbye";
+alert(message);
+message = "She's a great person!"
+var htmlSnippet = '<h1 class="specia">Important Headline</h1>';
+
+message = 'She\'s a great person!';
+htmlSnippet = "<h1 class=\"special\">Important Headline</h1>";
