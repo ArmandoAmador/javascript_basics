b952f8d38a19228ecf1d2d05208d3fc3973e9815
Armando Amador <arm.amador@gmail.com>
2015-07-14 23:08:55 -0400

    Work with how to get user input using prompt

--- variables/scripts.js
+++ variables/scripts.js
@@ -1,9 +1,2 @@
-var message = 'Hello';
-alert(message);
-message = "Goodbye";
-alert(message);
-message = "She's a great person!"
-var htmlSnippet = '<h1 class="specia">Important Headline</h1>';
-
-message = 'She\'s a great person!';
-htmlSnippet = "<h1 class=\"special\">Important Headline</h1>";
+var visitorName = prompt('What is your name?');
+console.log(visitorName);
